cbab1dc6f0b11515c825abeb25ab574dfb0ed5a9
MIKI Takushi <takushi-m@users.noreply.github.com>
2018-02-07 22:36:52 +0900

    hello world app

--- src/todo_clj/core.clj
+++ src/todo_clj/core.clj
@@ -1,6 +1,23 @@
-(ns todo-clj.core)
+(ns todo-clj.core
+  (:require [ring.adapter.jetty :as server]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defonce server (atom nil))
+
+(defn handler [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, world"})
+
+(defn start-server []
+  (when-not @server
+    (reset! server (server/run-jetty handler {:port 3000 :join? false}))))
+
+(defn stop-server []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
+(defn restart-server []
+  (when @server
+    (stop-server)
+    (start-server)))
